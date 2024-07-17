5c770b71a0d2268ab6494136c202e1a98a1f7521
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-17 18:40:46 +0530

    Using CSS Modules

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
+import { Link } from 'react-router-dom';
 import PageNav from '../components/PageNav';
+import AppNav from '../components/AppNav';
 
 function Home() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       <h1>Worldwise</h1>
+
+      <Link to="/app">Go ot the app</Link>
     </div>
   );
 }
