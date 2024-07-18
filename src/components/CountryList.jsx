8c121ea01d3dc21fcf897a63797209d59675438b
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-18 12:41:23 +0530

    fixes

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -25,7 +25,7 @@ function CountryList({ cities, isLoading }) {
   return (
     <ul className={styles.countryList}>
       {countries.map(country => (
-        <CountryItem country={country} key={country} />
+        <CountryItem country={country} key={country.country} />
       ))}
     </ul>
   );
