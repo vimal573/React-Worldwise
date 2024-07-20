1019ba7cc2eab6c8d1f6b425c316bf8058464de8
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-20 14:13:26 +0530

    State Management Context + useReducer

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -30,7 +30,7 @@ function CityItem({ city }) {
       >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
-        <time className={styles.date}>{formatDate(date)}</time>
+        <time className={styles.date}>({formatDate(date)})</time>
         <button className={styles.deleteBtn} onClick={handleCLick}>
           &times;
         </button>
