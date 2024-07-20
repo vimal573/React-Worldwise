9013faadffae4b2bf64711cdb049dfa8f23c79a5
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-20 15:23:24 +0530

    Adding Fake Authentication Protecting a route

---
+++ src/pages/ProctectedRoute.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/FakeAuthContext';
+import { useEffect } from 'react';
+
+function ProctectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      if (!isAuthenticated) navigate('/');
+    },
+    [isAuthenticated, navigate]
+  );
+
+  return isAuthenticated ? children : null;
+}
+
+export default ProctectedRoute;
